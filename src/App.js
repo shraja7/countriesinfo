03762975408aeac6b6ef7bfd0540d484c2434723
shraja7 <shraja7@gmail.com>
2023-03-22 11:27:41 -0500

    get countries information and display it

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
-import "./App.css";
-
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 function App() {
-  return <div className="App">Test</div>;
+  const [countries, setCountries] = useState([]);
+
+  //get countries data using useEffect and axios and store into countries state
+  useEffect(() => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      setCountries(response.data);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      Find Countries: <input type="text" />
+      {/* display the list of countries in the countries state */}
+      <ul>
+        {countries.map((country) => (
+          <li key={country.name.common}>{country.name.common}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
