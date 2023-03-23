95a5975b14c66ba26e44b19174690f445cd6a6db
shraja7 <shraja7@gmail.com>
2023-03-23 18:05:17 -0500

    add languages for country info

--- src/components/Country.js
+++ src/components/Country.js
@@ -1,12 +1,18 @@
 import React from "react";
 
 function Country({ country }) {
-  const { name, capital, population, flags } = country;
+  const { name, capital, population, flags, languages } = country;
   return (
     <div>
       <h2>{name.common}</h2>
       <p>Capital: {capital[0]}</p>
       <p>Population: {population}</p>
+      <h3>Languages:</h3>
+      <ul>
+        {Object.values(languages).map((language) => (
+          <li key={language}>{language}</li>
+        ))}
+      </ul>
       <img
         style={{ width: "100px", height: "auto" }}
         src={flags.svg}
