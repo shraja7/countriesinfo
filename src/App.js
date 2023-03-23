1cc809215ec90571489bde1c8d5b2c737478c99a
shraja7 <shraja7@gmail.com>
2023-03-23 16:33:37 -0500

    implement conditional rendering for coutnries and search filter

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 function App() {
   const [countries, setCountries] = useState([]);
+  const [filter, setFilter] = useState("");
 
   //get countries data using useEffect and axios and store into countries state
   useEffect(() => {
@@ -10,15 +11,28 @@ function App() {
     });
   }, []);
 
+  //handle the filter input
+  const handleFilter = (event) => {
+    console.log(event.target.value);
+    setFilter(event.target.value);
+  };
+  // filter the list of countries based on the search term
+  const filteredCountries = countries.filter((country) =>
+    country.name.common.toLowerCase().includes(filter.toLowerCase())
+  );
   return (
     <div className="App">
-      Find Countries: <input type="text" />
-      {/* display the list of countries in the countries state */}
-      <ul>
-        {countries.map((country) => (
-          <li key={country.name.common}>{country.name.common}</li>
-        ))}
-      </ul>
+      Find Countries: <input type="text" onChange={handleFilter} />
+      {/* display the list of countries based on the search term */}
+      {filteredCountries.length > 10 ? (
+        <div>Too many matches</div>
+      ) : (
+        <ul>
+          {filteredCountries.map((country) => (
+            <li key={country.name.common}>{country.name.common}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 }
